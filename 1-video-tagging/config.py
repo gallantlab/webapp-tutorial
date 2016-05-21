bf0f8a51e0de57b6de640b0e707b4ed3431aa435
Natalia Bilenko <nbilenko@airbears2-10-142-84-160.airbears2.1918.berkeley.edu>
2016-05-20 17:16:12 -0700

    fixed adding data directory

--- 1-video-tagging/config.py
+++ 1-video-tagging/config.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 VIDEO_DIR = os.path.join(BASE_DIR, 'video')
 DATA_DIR = os.path.join(BASE_DIR, 'data')
+if not os.path.isdir(DATA_DIR):
+	os.mkdir(DATA_DIR)
 DATA_FILE = "tagged.json"
 
 ALLOWED_EXTENSIONS = set(['mp4', 'ogv'])
